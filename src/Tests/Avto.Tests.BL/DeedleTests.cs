1ecde73c5688ad33a8f375fb07b8576e855bfe2e
Pavel Pontus <pavel_pontus@tut.by>
2022-06-17 22:54:56 +0300

    added doc

---
+++ src/Tests/Avto.Tests.BL/DeedleTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Avto.BL.Services.Exchange;
+using Avto.Tests.BL.Base;
+using Deedle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Avto.Tests.BL
+{
+    [TestClass]
+    public class DeedleTests : BaseServiceTests<ExchangeService>
+    {
+        [TestMethod]
+        public void Successfully_generate_deedle_frame()
+        {
+            var rnd = new Random();
+            var objects = Enumerable.Range(0, 10).Select(i =>
+                new { Key = "ID_" + i, Number = rnd.Next() });
+
+            // Create data frame with properties as column names
+            var dfObjects = Frame.FromRecords(objects);
+            dfObjects.Print();
+        }
+    }
+}
