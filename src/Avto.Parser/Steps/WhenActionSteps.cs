409b6dc9ae352747e2cf15d429cc66693b4a256a
Pavel Pontus <pavel_pontus@tut.by>
2024-08-30 18:41:30 +0300

    cleanup

---
+++ src/Avto.Parser/Steps/WhenActionSteps.cs
@@ -0,0 +1,180 @@
+﻿using System.Text;
+using AutoTestic.RethinkUI._Core;
+using TechTalk.SpecFlow;
+
+namespace AutoTestic.RethinkUI.Steps
+{
+    [Binding]
+    public class WhenActionSteps : BaseSteps
+    {
+        public WhenActionSteps(WebPage page) : base(page)
+        {
+        }
+
+        [When ("setup base url {(.*)}")]
+        public async Task WhenSetupBaseUrl(string baseUrl)
+        {
+            Console.WriteLine(GetStepName(baseUrl));
+            Page.BaseUrl = baseUrl;
+        }
+
+        [When("log {(.*)}")]
+        public async Task WhenLog(string cssSelector)
+        {
+            var html = await Page.SelectVisibleAndHiddenElement(cssSelector.NormalizeCssSelector()).Html();
+
+            Console.WriteLine($"Log: {html}");
+        }
+
+        [When(@"open {(.*)}")]
+        public async Task OpenPage(string url)
+        {
+            var isAbsoluteUrl = url.StartsWith("http");
+            var isRelativeUrl = !isAbsoluteUrl;
+            await Page.OpenUrl(url, isRelativeUrl);
+        }
+
+
+        [When(@"click {(.*)}")]
+        public async Task Click(string cssSelector)
+        {
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't click on element {cssSelector}, because it is not visible.");
+            }
+
+            await Page.SelectVisibleElement(cssSelector).Click();
+
+            await Page.WaitFor(1);
+        }
+
+
+        [When(@"type in {(.*)} text {(.*)}")]
+        public async Task TypeText(string cssSelector, string text)
+        {
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't type text for element {cssSelector}, because it is not visible.");
+            }
+            
+            await Page.SelectVisibleElement(cssSelector).Value(text);
+        }
+
+        
+        [When(@"type in {(.*)} random number")]
+        public async Task TypeRandomNumber(string cssSelector)
+        {
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't type random number for element {cssSelector}, because it is not visible.");
+            }
+
+
+            var element = Page.SelectVisibleElement(cssSelector);
+            var currentText = await element.Value();
+            int randomNumber;
+
+            do
+            {
+                randomNumber = new Random().Next(1, 100);
+            } while (randomNumber.ToString() == currentText);
+
+
+            await Page.SelectVisibleElement(cssSelector).Value(randomNumber.ToString());
+        }
+
+        
+        [When(@"type in {(.*)} plus one")]
+        public async Task TypeDoubleNumber(string cssSelector)
+        {
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't type random number for element {cssSelector}, because it is not visible.");
+            }
+
+
+            var element = Page.SelectVisibleElement(cssSelector);
+            var currentText = await element.Value();
+            var currentNumber = int.Parse(currentText);
+            var newNumber = currentNumber + 1;
+
+            await Page.SelectVisibleElement(cssSelector).Value(newNumber.ToString());
+        }
+
+        
+        [When(@"type in {(.*)} remembered {(.*)}")]
+        public async Task EnterRememberedText(string cssSelector, string name)
+        {
+            var text = Page.Remember[name];
+
+            if (text.IsNullOrEmpty())
+            {
+                throw new Exception($"Remembered text {name} is null or empty.");
+            }
+            
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't type into element {cssSelector}, because it is not visible.");
+            }
+
+            await Page.SelectVisibleElement(cssSelector).Value(text);
+        }
+
+
+        [When(@"type in {(.*)} enter")]
+        public async Task ClickEnter(string cssSelector)
+        {
+            var isElementVisible = await Page.WaitUntilElementIsVisible(cssSelector);
+            if (!isElementVisible)
+            {
+                await Page.MakeScreenShot();
+                throw new Exception($"I can't type enter for element {cssSelector}, because it is not visible.");
+            }
+            
+            await Page.SelectVisibleElement(cssSelector).PressKeyEnter();
+            await Page.WaitFor(1);
+        }
+
+
+        [When(@"remember text in {(.*)} as {(.*)}")]
+        public async Task RememberText(string cssSelector, string name)
+        {
+            var value = await Page.SelectVisibleAndHiddenElement(cssSelector).Value();
+            Page.Remember[name] = value;
+            Console.WriteLine($"Remembered {value} as {name}");
+        }
+
+
+        [When(@"run script {(.*)}")]
+        public async Task RunScript(string script)
+        {
+            await Page.RunScript(script);
+            Console.WriteLine($"Run script {script}");
+        }
+
+        
+        [When(@"remember text in script {(.*)} as {(.*)}")]
+        public async Task RememberTextFromScript(string script, string name)
+        {
+            var value = await Page.RunScriptAndGetValue<string>(script);
+            Page.Remember[name] = value;
+            Console.WriteLine($"Remembered {value} as {name}");
+        }
+
+        [When(@"make screenshot")]
+        public async Task MakeScreenshot()
+        {
+            await Page.MakeScreenShot();
+        }
+    }
+}
---
+++ src/Avto.Parser/_Core/Settings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestic.RethinkUI._Core
+{
+    public class Settings
+    {
+        public string BaseUrl
+        {
+            get
+            {
+#if DEBUG
+                return "https://albion-test7.rethinkportal.com";
+#endif
+#pragma warning disable CS0162
+                return "https://albion-test7.rethinkportal.com";
+#pragma warning restore CS0162
+            }
+        }
+
+        public string Auth0Url => "https://rethink-dev.eu.auth0.com";
+        public string Auth0ClientId => "k60FvtApWNrCszAWPG0uu2mc0eeCUaVA";
+        public string Auth0ClientSecret => "_-aikSxWiUuIfys1I169gvlgesd_gKaB9YJ8ytUlWFwqLahhH7SCGDzJUTmpBoeP";
+        public string Auth0Db => "RethinkUserDb";
+        public string Auth0Audience => "https://p3-test-01-Rethink";
+
+        public string SellerAdminAuthLogin => "grin2@qa.qa";
+        public string SellerAdminAuthPassword => "Grin2@qa.qa";
+
+        public string CustomerAdminAuthLogin => "customer.admin@auto.testic.com";
+        public string CustomerAdminAuthPassword => "!123Qqwe";
+
+        public string SuperAdminAuthLogin => "pasha.parrrker@gmail.com";
+        public string SuperAdminAuthPassword => "!123Qqwe";
+
+        public string RethinkBackApiUrl = "https://appxite-rethink-back-api-p3-test-01.azurewebsites.net/api/";
+        public int RethinkBackApiRequestTimeoutInSec = 60;
+
+        public Guid CustomerOrganizationId => new Guid("B950BB7C-0A8F-46E4-8B2E-FEDF7B74E503");
+        public string SellerPlatformUrl => "https://albion-test7.rethinkportal.com";
+    }
+}
